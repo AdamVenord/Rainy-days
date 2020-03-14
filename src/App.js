05760d633ee90d11fc880c3ef84153fd8becebfa
Adam Venord <AdamVenord7@gmail.com>
2020-03-13 19:33:48 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const api = {
+  key: "5f3f7c14321586f0c21fa355c5717aeb",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August",
+      "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
+      "Saturday"];
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`
+  }
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeHolder="Search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">Salt Lake City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
